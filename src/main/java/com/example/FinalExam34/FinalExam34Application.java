c82a258f078bd0a7d4a45af4aad48e357a835daa
maulan1208 <nguyenlan12082003@gmail.com>
2023-08-18 21:51:02 +0700

    new project and connect database

---
+++ src/main/java/com/example/FinalExam34/FinalExam34Application.java
@@ -0,0 +1,13 @@
+package com.example.FinalExam34;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExam34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalExam34Application.class, args);
+	}
+
+}
